44982e98e80f0709e9e4aa6641aa199eee7624df
soualexoliveira <alexs.oliveira1998@gmail.com>
2023-03-04 02:30:02 -0300

    up quase pronto - falta posts, site e app

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 'use strict'
+let imagens = document.querySelectorAll('.projeto_imgs');
+let modal = document.querySelector('.modal');
+let modalImg = document.querySelector('#modal_img');
+let btClose = document.querySelector('#bt_close');
+let srcVal="";
+
+for(let i = 0; i<imagens.length; i++){
+    imagens[i].addEventListener('click', function(){
+        srcVal = imagens[i].getAttribute('src');
+        modalImg.setAttribute('src', srcVal);
+        modal.classList.toggle('modal_active');
+    });
+}
+
+btClose.addEventListener('click', function(){
+    modal.classList.toggle('modal_active');
+});
