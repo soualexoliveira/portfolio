fa71a45ca87c99fb323e8d532d0ab8c04accbb4c
Alex Oliveira <alexs.oliveira1998@gmail.com>
2025-01-13 13:13:45 -0300

    Update transition-page.js

--- js/transition-page.js
+++ js/transition-page.js
@@ -1,3 +1,5 @@
+let isNavigating = false; // Flag para controlar a navegação
+
 document.addEventListener('DOMContentLoaded', () => {
     // Adiciona a classe de entrada ao carregar a página
     document.body.classList.add('fade-in');
@@ -9,6 +11,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
 			if (this.classList.contains('no-fade')) return; // classe pra evitar a animação em elementos especificos
 
+            if (isNavigating) return; // Se a navegação já está em andamento, não faz nada
+ 
+            isNavigating = true; // Marca que está navegando
+
 			event.preventDefault(); // Previne o redirecionamento imediato
 			const href = this.href;
 
@@ -19,4 +25,9 @@ document.addEventListener('DOMContentLoaded', () => {
 			}, 300); // Tempo igual ao da transição no CSS
 		});
 	});
+});
+
+// Resetando o estado quando a página terminar de carregar
+window.addEventListener('load', () => {
+    isNavigating = false; // Permite novas navegações
 });
